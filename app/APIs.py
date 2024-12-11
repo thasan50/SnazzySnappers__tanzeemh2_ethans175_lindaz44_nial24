364856a138c7427ac35c44d0dab4abe92123e75d
Tanzeem Hasan <thasan50@stuy.edu>
2024-12-11 10:55:15 -0500

    Current changes

--- app/APIs.py
+++ app/APIs.py
@@ -9,20 +9,34 @@ import urllib.parse
 import urllib.request
 import json
 
-def fetch_worldpop_data(country_iso3):
-    url = f"https://hub.worldpop.org/rest/data/pop/wpgp?iso3={country_iso3}"
-    headers = {"User-Agent": "Mozilla/5.0"}
+def fetch_city_pop(city_name):
     try:
-        request = Request(url, headers=headers)
-        response = urlopen(request)
-        data = json.loads(response.read().decode("utf-8"))
-        print(json.dumps(data, indent=4))
-    except Exception as e:
+        with open(key_file_path, "r") as file:
+            api_key = file.read().strip()
+        if not api_key:
+            raise ValueError("API key file is empty. Please add a valid API key.")
+        url = "https://api.api-ninjas.com/v1/"
+        params = {
+            "city": city_name,
+            "X-Api-Key": api_key
+        }
+        response = request.get(url, params = params)
+        if response.status_code == 200:
+            fonts_data = response.json()
+            print(json.dumps(city_data, indent=4))
+        else:
+            print(f"Error: Unable to fetch data (Status code: {response.status_code})")
+            print(f"Response: {response.text}")
+
+    except FileNotFoundError:
+        print(f"Error: The file '{key_file_path}' was not found. Please create it and add your API key.")
+    except ValueError as ve:
+        print(f"Error: {ve}")
+    except request.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
 def fetch_google_fonts():
     key_file_path = os.path.join("keys", "key_GoogleFonts.txt")
-
     try:
         with open(key_file_path, "r") as file:
             api_key = file.read().strip()
@@ -32,7 +46,7 @@ def fetch_google_fonts():
         params = {
             "key": api_key
         }
-        response = requests.get(url, params=params)
+        response = request.get(url, params=params)
         if response.status_code == 200:
             fonts_data = response.json()
             print(json.dumps(fonts_data, indent=4))
@@ -44,7 +58,7 @@ def fetch_google_fonts():
         print(f"Error: The file '{key_file_path}' was not found. Please create it and add your API key.")
     except ValueError as ve:
         print(f"Error: {ve}")
-    except requests.exceptions.RequestException as e:
+    except request.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
 def fetch_openweather_data(city_name):
@@ -60,7 +74,7 @@ def fetch_openweather_data(city_name):
             "appid": api_key,
             "units": "metric"
         }
-        response = requests.get(url, params=params)
+        response = request.get(url, params=params)
         if response.status_code == 200:
             weather_data = response.json()
             print(json.dumps(weather_data, indent=4))
@@ -72,7 +86,7 @@ def fetch_openweather_data(city_name):
         print(f"Error: The file '{key_file_path}' was not found. Please create it and add your API key.")
     except ValueError as ve:
         print(f"Error: {ve}")
-    except requests.exceptions.RequestException as e:
+    except request.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
 def fetch_visualcrossing_data(location):
@@ -88,7 +102,7 @@ def fetch_visualcrossing_data(location):
             "key": api_key,
             "unitGroup": "metric"
         }
-        response = requests.get(full_url, params=params)
+        response = request.get(full_url, params=params)
         if response.status_code == 200:
             weather_data = response.json()
             print(json.dumps(weather_data, indent=4))
@@ -100,7 +114,7 @@ def fetch_visualcrossing_data(location):
         print(f"Error: The file '{key_file_path}' was not found. Please create it and add your API key.")
     except ValueError as ve:
         print(f"Error: {ve}")
-    except requests.exceptions.RequestException as e:
+    except request.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 
 def fetch_earthquake_data():

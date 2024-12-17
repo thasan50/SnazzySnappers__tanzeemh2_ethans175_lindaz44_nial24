4a703b745488025c687a0555856fdf5565a916a7
ethannsie <esie50@stuy.edu>
2024-12-17 01:10:36 -0500

    added userhistory and styling

--- app/db.py
+++ app/db.py
@@ -17,7 +17,7 @@ def setup():
     # these are unique per DB, but the DBs are still connected through userID and search_type
     # note that we may want to create another column to detail the timestamps of the data itself from the APIs
     c.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, created_at TEXT, last_login TEXT);")
-    c.execute("CREATE TABLE IF NOT EXISTS userHistory (history_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, search_type TEXT, search_time TEXT);")
+    c.execute("CREATE TABLE IF NOT EXISTS userHistory (history_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, search_type TEXT, location_name TEXT, search_time TEXT);")
     c.execute("CREATE TABLE IF NOT EXISTS weather (weather_id INTEGER PRIMARY KEY, location_name TEXT, latitude REAL, longitude REAL, temperature REAL, humidity INTEGER, weather TEXT, wind_speed REAL, search_time TEXT);")
     c.execute("CREATE TABLE IF NOT EXISTS weatherHistory (history_id INTEGER PRIMARY KEY AUTOINCREMENT, location_name TEXT, latitude REAL, longitude REAL, year INTEGER, avg_temperature REAL, avg_precipitation REAL, high_temperature REAL, low_temperature REAL, search_time TEXT);")
     c.execute("CREATE TABLE IF NOT EXISTS earthquakes (earthquake_id INTEGER PRIMARY KEY AUTOINCREMENT, location_name TEXT, latitude REAL, longitude REAL, magnitude REAL, depth REAL, description TEXT, search_time TEXT);")
@@ -56,11 +56,11 @@ def getUserID(username):
     else:
         return -1
 
-def updateUserHistory(username, search_type, current_time):
+def updateUserHistory(username, search_type, location_name, current_time):
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
     userID = getUserID(username)
-    c.execute("INSERT INTO userHistory (user_id, search_type, search_time) VALUES (?, ?, ?)", (userID, search_type, current_time))
+    c.execute("INSERT INTO userHistory (user_id, search_type, location_name, search_time) VALUES (?, ?, ?, ?)", (userID, search_type, location_name, current_time))
     db.commit()
     db.close()
 
@@ -68,7 +68,8 @@ def logWeather(weather_id, username, location_name, latitude, longitude, tempera
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    updateUserHistory(username, "weather", current_time)
+    if getTableData("userHistory", "search_time", current_time) == -1:
+        updateUserHistory(username, "weather", location_name, current_time)
     c.execute("INSERT INTO weather (weather_id, location_name, latitude, longitude, temperature, humidity, weather, wind_speed, search_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (weather_id, location_name, latitude, longitude, temperature, humidity, weather, wind_speed, current_time))
     db.commit()
     db.close()
@@ -77,7 +78,7 @@ def logWeatherHistory(username, location_name, latitude, longitude, year, avg_te
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    updateUserHistory(username, "weatherHistory", current_time)
+    updateUserHistory(username, "weatherHistory", location_name, current_time)
     c.execute("INSERT INTO weatherHistory (location_name, latitude, longitude, year, avg_temperature, avg_precipitation, high_temperature, low_temperature, search_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (location_name, latitude, longitude, year, avg_temperature, avg_precipitation, high_temperature, low_temperature, current_time))
     db.commit()
     db.close()
@@ -86,7 +87,7 @@ def logEarthquakes(username, location_name, latitude, longitude, magnitude, dept
     db = sqlite3.connect(DB_FILE, check_same_thread=False)
     c = db.cursor()
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    updateUserHistory(username, "earthquakes", current_time)
+    updateUserHistory(username, "earthquakes", location_name, current_time)
     c.execute("INSERT INTO earthquakes (location_name, latitude, longitude, magnitude, depth, description, search_time) VALUES (?, ?, ?, ?, ?, ?, ?)", (location_name, latitude, longitude, magnitude, depth, description, current_time))
     db.commit()
     db.close()
@@ -99,6 +100,18 @@ def getTableData(table, valueType, value):
     # make sure that this all exists
     c.execute("SELECT * FROM " + table + " WHERE " + valueType + " = ?", (value,))
     result = c.fetchone()
+    db.close()
+    # check in case there is an error in fetching data
+    if result:
+        return result
+    else:
+        return -1
+
+def getAllHistory(userID):
+    db = sqlite3.connect(DB_FILE, check_same_thread=False)
+    c = db.cursor()
+    c.execute("SELECT * FROM userHistory WHERE user_id = ?", (userID,))
+    result = c.fetchall()
     db.close()
     # check in case there is an error in fetching data
     if result:
